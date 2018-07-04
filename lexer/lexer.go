f120e858a326c567b35d979e5262cdf45f951f50
ngiyshhk <nagai_yoshihiko_gn@cyberagent.co.jp>
2018-07-04 10:56:34 +0900

    add const

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -112,3 +112,11 @@ func (l *Lexer) skipWhitespace() {
 		l.readChar()
 	}
 }
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
