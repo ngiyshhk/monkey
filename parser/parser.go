0cb4bbe3e608ce16f49148d59a863882579f9eaf
ngiyshhk <nagai_yoshihiko_gn@cyberagent.co.jp>
2018-07-31 13:44:23 +0900

    feat: implement parse return (chapter 2.5)

--- parser/parser.go
+++ parser/parser.go
@@ -58,6 +58,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -100,4 +102,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてるらしいよ
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
