f9a544ee5f407afdf2db55e0655d802efaa275eb
ngiyshhk <ngiys@icloud.com>
2018-12-17 22:03:48 +0900

    feat: implement array index

--- parser/parser.go
+++ parser/parser.go
@@ -17,6 +17,7 @@ const (
 	PRODUCT    // *
 	PREFIX     // -X or !X
 	CALL       // myFunction(X)
+	INDEX      // array[index]
 )
 
 type Parser struct {
@@ -61,6 +62,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	// curTokenとpeekTokenのセットのため2回呼ぶ
 	p.nextToken()
@@ -416,6 +418,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 func (p *Parser) peekPrecedence() int {
@@ -468,4 +471,17 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 	}
 
 	return list
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
+	return exp
 }
